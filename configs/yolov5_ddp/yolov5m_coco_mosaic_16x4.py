6ac7e5737aea801bd9e8b5a2d0cb9a846621f136
Gefan Zhang <gefan.zhang@cowarobot.com>
2021-03-05 17:55:49 +0800

    refactor yolov5 ddp configs

--- configs/yolov5_ddp/yolov5m_coco_mosaic_16x4.py
+++ configs/yolov5_ddp/yolov5m_coco_mosaic_16x4.py
@@ -16,7 +16,8 @@ optimizer = dict(lr=0.01)
 
 data = dict(samples_per_gpu=16, workers_per_gpu=2)
 
-optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
+optimizer_config = dict(
+    _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
 fp16 = dict(
     loss_scale=dict(
-        init_scale=2**22, mode='dynamic', scale_factor=2., scale_window=1000))
+        init_scale=2**16, mode='dynamic', scale_factor=2., scale_window=1000))
